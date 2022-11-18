8b018e3e3ff8c3154b1a97efd2fe9dd795176af6
Pavel <rpv0001@yandex.ru>
2022-11-18 18:40:28 +0500

    first commit

---
+++ Sender/main_worker.py
@@ -0,0 +1,42 @@
+import schedule
+import datetime
+
+from forming_and_sending_messages import forming_and_sending_messages
+from core.models import MailingList
+
+
+def zeromin(minutes):
+    if minutes < 10:
+        return f'0{minutes}'
+    return str(minutes)
+
+
+def today_messages():
+    """ Формирование списка сообщений """
+    today = datetime.date.today()
+    messages_to_sent = MailingList.objects.all()
+    today_message = list()
+    for message_to_sent in messages_to_sent:
+        time_to_sent = message_to_sent.date_of_mailing.timetuple()
+        message_day = datetime.date(time_to_sent[0], time_to_sent[1], time_to_sent[2])
+        time_to_stop = message_to_sent.date_of_end_mailing.timetuple()
+        stop_message_day = datetime.date(time_to_stop[0], time_to_stop[1], time_to_stop[2])
+        if message_day <= today < stop_message_day:
+            today_message.append(message_to_sent)
+    return today_message
+
+
+def main():
+    while True:
+        messages_to_sent = today_messages()
+        for message_to_sent in messages_to_sent:
+            time_to_sent = message_to_sent.date_of_mailing.timetuple()
+            message_hours = f'{time_to_sent[3]}:{zeromin(time_to_sent[4])}'
+            schedule.every().day.at(message_hours).do(
+                forming_and_sending_messages, message_to_sent=message_to_sent
+            )
+            schedule.clear()
+
+
+if __name__ == '__main__':
+    main()
---
+++ core/models.py
@@ -0,0 +1,114 @@
+from django.core.validators import RegexValidator
+from django.db import models
+
+
+class MailingList(models.Model):
+    """Модель рассылки"""
+    COD_NUMBER = 'Код мобильного оператора'
+    TAG = 'Тэг'
+
+    CLIENT_FILTER = [
+        (COD_NUMBER, 'Код мобильного оператора'),
+        (TAG, 'Тэг')
+    ]
+
+    date_of_mailing = models.DateTimeField(
+        null=False,
+        verbose_name='дата и время запуска рассылки'
+    )
+    text_of_message = models.TextField(
+        null=False,
+        verbose_name='текст сообщения для доставки клиенту'
+    )
+    client_properties_filter = models.CharField(
+        max_length=64,
+        choices=CLIENT_FILTER,
+        verbose_name='Фильтр свойств клиентов'
+    )
+    client_properties = models.CharField(
+        max_length=8,
+        verbose_name='Значение выбранного свойства клиента'
+    )
+    date_of_end_mailing = models.DateTimeField(
+        null=False, verbose_name='дата и время окончания рассылки'
+    )
+
+
+    class Meta:
+        verbose_name = 'Рассылкa'
+        verbose_name_plural = 'Рассылки'
+
+
+    def __str__(self):
+        return f'{self.id} // {self.date_of_mailing}'
+
+
+class Client(models.Model):
+    """Модель клиента"""
+
+    reg_phone_number = RegexValidator(regex=r'^7\w{10}$',
+                                      message='номер телефона клиента '
+                                      )
+    phone_number = models.CharField(max_length=12,
+                                    verbose_name='Мобильный телефон',
+                                    validators=[reg_phone_number])
+    mobile_operator_code = models.IntegerField(
+        null=False,
+        verbose_name='код мобильного оператора'
+    )
+    tag = models.CharField(
+        unique=False,
+        max_length=16,
+        verbose_name='тег (произвольная метка)'
+    )
+    timezone = models.IntegerField(
+        verbose_name='часовой пояс'
+    )
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+    def __str__(self):
+        return f'{self.id}'
+
+
+class Message(models.Model):
+    """Модель сообщения"""
+
+    class Meta:
+        verbose_name = 'Сообщениe'
+        verbose_name_plural = 'Сообщения'
+
+    SENT = 'Отправлено'
+    NOT_SENT = 'Не отправлено'
+    PROBLEM = 'Возникла проблема'
+    STATUS = [
+        (SENT, 'Отправлено'),
+        (NOT_SENT, 'Не отправлено'),
+        (PROBLEM, 'Возникла проблема')
+    ]
+
+    date_of_create = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='дата и время создания (отправки)',
+    )
+    status = models.CharField(
+        max_length=32,
+        choices=STATUS,
+        default=NOT_SENT,
+        verbose_name='статус отправки'
+    )
+    id_of_mailing_list = models.ForeignKey(
+        MailingList,
+        on_delete=models.CASCADE,
+        verbose_name='id рассылки, в рамках которой было отправлено сообщение',
+    )
+    id_of_client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        verbose_name='id клиента',
+    )
+
+    def __str__(self):
+        return f'Сообщение {self.date_of_create} из {self.id_of_mailing_list}'
